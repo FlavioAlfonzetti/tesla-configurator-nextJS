4cdf3718f555973eb8562ac27e6387b7d423085c
flavioalfonzetti <flavioalfonzetti@gmail.com>
2024-12-27 11:45:38 +0100

    fixed img logo in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 const Header = () => {
   return (
     <header className="flex justify-between items-center px-10 py-6 mt-16">
-      <img src="./images/logo.svg" alt="Tesla" width="120" height="24" />
+      <Image src="/images/logo.svg" alt="Tesla" width={120} height={24} />
       <button>
         <i className="fa-solid fa-globe"></i>{" "}
         <span className="font-bold">US</span>
